e191d39e91829be01a19e9f56c20c8198ef27429
tan nguyen <xin81@nurfuerspam.de>
2015-04-10 18:12:15 +0200

    update bubblesort.c

--- bubblesort/bubblesort.c
+++ bubblesort/bubblesort.c
@@ -6,12 +6,12 @@ void swap(int* a, int* b){
 }
 
 void bubblesort(int* array, const int MAX){
-	for(int i=0; i < MAX; i++){
+for(int i=0; i < MAX; i++){
    for(int j=(i+1); j <= (MAX-1); j++){
-  	if(array[i] > array[j]){
- 			swap(&array[i], &array[j]);
-    }
+      if(array[i] > array[j]){
+	swap(&array[i], &array[j]);
+      }
    }
-  }
+}
 }
 
